a521ec4fda16a142ef1eb45d81fa949c744cea45
AndreyMeshkow <meshkow.andrei@gmail.com>
2020-05-01 17:16:13 +0300

    feat: add post.service

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyC-oyzKxI503TgJp6szc42Yozj6szU1nvw'
+  apiKey: 'AIzaSyC-oyzKxI503TgJp6szc42Yozj6szU1nvw',
+  fbDbUrl: 'https://angular-blog-e448d.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyC-oyzKxI503TgJp6szc42Yozj6szU1nvw'
+  apiKey: 'AIzaSyC-oyzKxI503TgJp6szc42Yozj6szU1nvw',
+  fbDbUrl: 'https://angular-blog-e448d.firebaseio.com'
 };
 
 /*
